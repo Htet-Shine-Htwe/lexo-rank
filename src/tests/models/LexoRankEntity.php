99b651c3cff9fe656bd1c6885de66a69e2367593
Htet Shine Htwe <htetshine.coin@gmail.com>
2025-02-23 22:50:59 +0630

    first init and dev

---
+++ src/tests/models/LexoRankEntity.php
@@ -0,0 +1,10 @@
+<?php
+namespace Dede\Lexorank\tests\models;
+
+use Dede\Lexorank\LexoRankTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class LexoRankEntity extends Model
+{
+    use LexoRankTrait;
+}
